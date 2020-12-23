afd299bfbcc72a3f68b3acb2870d96be15f88e05
jonathan <noractron@gmail.com>
2020-12-22 21:53:34 -0500

    prueba

--- Servidor/conexion/server.js
+++ Servidor/conexion/server.js
@@ -1,4 +1,4 @@
-const url = "mongodb+srv://truequemundo:TruequeMundo12345@truequemundo.xrdjb.mongodb.net/truequeMundo?retryWrites=true&w=majority";
+const url = "mongodb+srv://truequemundo:Jose12345@@truequemundo.xrdjb.mongodb.net/truequeMundo?retryWrites=true&w=majority";
 
 // mongodb+srv://norac:12345@cluster0.6evyd.mongodb.net/truequeMundo?retryWrites=true&w=majority
 var PORT= process.env.PORT || 3000;
